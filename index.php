13a9fdf64bfc519f694e46e53298f91f99828fc4
IsraelMerlyn <merlyn142a@gmail.com>
2020-05-04 13:33:04 -0500

    añadiendo nuestro index

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+      <div class="contenedor">
+        <div id="marca">
+          <h1><span class="resaltado">Bienvenidos a</span> My Tec</h1>
+        </div>
+        <nav>
+          <ul>
+            <li class="actual"><a href="index.html">Inicio</a></li>
+            <li><a href="nosotros.html">Nosotros</a></li>
+            <li><a href="servicios.html">Servicios</a></li>
+            <li><a href="contacto.html">Contacto</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <section id="cabecera">
+      <div class="contenedor">
+        <h1>Bienvenidos a MyTec</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem interdum condimentum.</p>
+      </div>
+    </section>
+
+    <section id="boletin">
+      <div class="contenedor">
+        <h1>Subscríbete a nuestro boletín</h1>
+        <form>
+          <input type="email" placeholder="Ingrese el Email...">
+          <button type="submit" class="boton1">Subscríbete</button>
+        </form>
+      </div>
+    </section>
+
+    <section id="cajas">
+      <div class="contenedor">
+        <div class="caja">
+          <img src="./img/logo_html.png">
+          <h3>Programacion Web HTML5</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
+        </div>
+        <div class="caja">
+          <img src="./img/logo_css.png">
+          <h3>Ingeneria de software</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
+        </div>
+        <div class="caja">
+          <img src="./img/logo_brush.png">
+          <h3>Jovenes Escribiendo el futuro</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies</p>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <p>Israel, Esperanza, Claudia &copy; 2020</p>
+    </footer>
+</body>
+</html>
